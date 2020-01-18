8a9b03be599109fb9a39db46d7af6fc1197f42a9
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-01-18 12:22:41 -0800

    Release v0.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExDeribit.MixProject do
   def project do
     [
       app: :ex_deribit,
-      version: "0.0.3",
+      version: "0.0.4",
       elixir: "~> 1.7",
       package: package(),
       start_permanent: Mix.env() == :prod,
