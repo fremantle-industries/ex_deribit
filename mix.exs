128cb0921c455438181fddacce6391f067c444dd
Alex <alex+git@fremantle.io>
2021-06-02 14:31:13 -0700

    Release v0.0.9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExDeribit.MixProject do
   def project do
     [
       app: :ex_deribit,
-      version: "0.0.8",
+      version: "0.0.9",
       elixir: "~> 1.10",
       package: package(),
       start_permanent: Mix.env() == :prod,
